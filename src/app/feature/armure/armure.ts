ba26742ec7b1722b382407d9200de7c7ccfa6a31
maxime.coustes <maxime.coustes@hotmail.fr>
2019-02-13 09:19:26 +0100

    création branch featreu/armure et armure.ts

---
+++ src/app/feature/armure/armure.ts
@@ -0,0 +1,13 @@
+export class Armure {
+/*     id: string;
+    categorie: string;
+    nom: string;
+    prix: string;
+    degats: string;
+    critique: string;
+    portee?: string;
+    poids: string;
+    special?: string; */
+}
+// ajouter la description des armes
+// créer une table munitions ?
